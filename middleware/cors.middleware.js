7e6c04d7b4bae33025af5e79019993f87c1851ef
Ilia Lisov <ilya.lisov.22@gmail.com>
2023-03-29 20:06:52 +0400

    CORS fix - 2

--- middleware/cors.middleware.js
+++ middleware/cors.middleware.js
@@ -1,5 +1,5 @@
 function cors(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://phenomenal-daffodil-52450e.netlify.app")
+    res.header("Access-Control-Allow-Origin", "https://gardener-store.netlify.app")
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE")
     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
     res.header("Access-Control-Allow-Credentials", "true")
