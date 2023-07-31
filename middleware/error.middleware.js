5a4bff730a89368b8c92a58523809d9f26670003
Ilia Lisov <ilya.lisov.22@gmail.com>
2023-08-01 00:37:26 +0400

    error research

--- middleware/error.middleware.js
+++ middleware/error.middleware.js
@@ -3,6 +3,7 @@ const ApiError = require('../exceptions/api-error')
 function error(err, req, res, next) {
     
     process.env.NODE_ENV === 'development' && console.log(err)
+    console.log(err)
     if (err instanceof ApiError) {
         return res.status(err.status).json({message: err.message, errors: err.errors})
     }
